fb2f7fecdc753b538c55a701ce3ccfb3926328f5
Angel Diaz <warydiaz@hotmail.com>
2023-06-19 19:53:04 +0200

    changing endpoint of match to team. adding endpoint to get by id

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
-import match from "./routers/match.js";
+import team from "./routers/team.js";
 
 //to use the file .env
 dotenv.config();
@@ -13,7 +13,7 @@ app.use(express.json());
 //to log request http
 app.use(morgan("dev"));
 
-app.use("/match", match);
+app.use("/team", team);
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`The API listening on: ${process.env.SERVER_PORT}`);
